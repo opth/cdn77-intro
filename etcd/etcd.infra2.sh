33a85caa8dee74362b9eebb628e30d0a38961457
MrReactive <ondrap266@gmail.com>
2023-03-06 13:07:48 +0100

    fix bracket position error

--- etcd/etcd.infra2.sh
+++ etcd/etcd.infra2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-nohup etcd --name infra2 --initial-advertise-peer-urls http://172.77.0.12:2380 \
+( ( nohup etcd --name infra2 --initial-advertise-peer-urls http://172.77.0.12:2380 \
   --listen-peer-urls http://172.77.0.12:2380 \
   --listen-client-urls http://172.77.0.12:2379,http://127.0.0.1:2379 \
   --advertise-client-urls http://172.77.0.12:2379 \
@@ -7,4 +7,4 @@ nohup etcd --name infra2 --initial-advertise-peer-urls http://172.77.0.12:2380 \
   --initial-cluster infra0=http://172.77.0.10:2380,infra1=http://172.77.0.11:2380,infra2=http://172.77.0.12:2380 \
   --initial-cluster-state new \
   --listen-metrics-urls http://172.77.0.12:2381 \
-  --metrics extensive &
+  --metrics extensive ) & )
